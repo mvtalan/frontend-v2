5109c17c9f7577b115f331b002ff8a28121af713
mvtalan <mvtalan@gmail.com>
2024-11-15 16:30:53 -0700

    working authenticated routes

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,19 +1,31 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+import UsernameMenu from "./UserNameMenu";
 
 const MainNav = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
 
     return (
-        <Button 
-            variant="ghost" 
-            className="font-bold hover:text-orange-500 hover:bg:white"
-            onClick={async () => await loginWithRedirect() }
-        >
-            Login
-        </Button>
-    )
-
-}
+        <span>
+            {isAuthenticated ? (
+                <>
+                <Link to="/order-status" className="font-bold hover:text-orange-500">
+                    Order Status
+                </Link>
+                <UsernameMenu />
+                </>
+            ) : (
+                <Button 
+                    variant="ghost" 
+                    className="font-bold hover:text-orange-500 hover:bg:white"
+                    onClick={async () => await loginWithRedirect() }
+                >
+                    Login
+                </Button>
+            )}
+        </span>
+    );
+};
 
 export default MainNav;
