91fd4af95247461b84cc7999e225d7f51b8402be
mvtalan <mvtalan@gmail.com>
2024-11-26 21:13:02 -0700

    with working checkout page

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { Auth0Provider } from "@auth0/auth0-react";
+import { AppState, Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -17,9 +17,9 @@ const Auth0ProviderWithNavigate = ({ children } : Props) => {
         throw new Error("Unable to initialize auth.");
     }
 
-    const onRedirectCallback = () => {
+    const onRedirectCallback = (appState?: AppState) => {
         // console.log("USER", user);
-        navigate( "/auth-callback");
+        navigate(appState?.returnTo || "/auth-callback");
     };
 
     return (
--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -31,10 +31,17 @@ const formSchema = z.object({
     currentUser: User;
     onSave: (userProfileData: UserFormData) => void;
     isLoading: boolean;
-
+    title?: string;
+    buttonText?: string;
   };
 
-  const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {
+  const UserProfileForm = ({ 
+    onSave, 
+    isLoading, 
+    currentUser,
+    title = "User Profile",
+    buttonText = "Submit", 
+  }: Props) => {
     const form = useForm<UserFormData>({
       resolver: zodResolver(formSchema),
       defaultValues: currentUser,
@@ -51,7 +58,7 @@ const formSchema = z.object({
             className="space-y-4 bg-gray-50 rounded-lg md:p-10"
           >
             <div>
-              <h2 className="text-2xl font-bold">Title</h2>
+              <h2 className="text-2xl font-bold">{title}</h2>
               <FormDescription>
                 View and change your profile information here
               </FormDescription>
@@ -128,7 +135,7 @@ const formSchema = z.object({
               <LoadingButton />
             ) : (
               <Button type="submit" className="bg-orange-500">
-                Submit
+                {buttonText}
               </Button>
             )}
           </form>
