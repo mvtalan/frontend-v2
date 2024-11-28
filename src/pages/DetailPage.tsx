58e66eb7d617895096025c8e55551cde4d1909bd
mvtalan <mvtalan@gmail.com>
2024-11-28 14:38:10 -0700

    working stripe payment

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -9,6 +9,7 @@ import { useParams } from "react-router-dom";
 import { MenuItem as MenuItemType } from "../types";
 import CheckoutButton from "@/components/CheckoutButton";
 import { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import { useCreateCheckoutSession } from "@/api/OrderApi";
 
 export type CartItem = {
     _id: string;
@@ -20,8 +21,8 @@ export type CartItem = {
 const DetailPage = () => {
     const { restaurantId } = useParams();
     const { restaurant, isLoading } = useGetRestaurant(restaurantId);
-    // const { createCheckoutSession, isLoading: isCheckoutLoading } =
-    //   useCreateCheckoutSession();
+    const { createCheckoutSession, isLoading: isCheckoutLoading } =
+      useCreateCheckoutSession();
 
     const [cartItems, setCartItems] = useState<CartItem[]>(() => {
         const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
@@ -79,29 +80,29 @@ const DetailPage = () => {
     };
 
     const onCheckout = async (userFormData: UserFormData) => {
-        console.log("userFormData", userFormData)
-        // if (!restaurant) {
-        //   return;
-        // }
+        // console.log("userFormData", userFormData)
+        if (!restaurant) {
+          return;
+        }
     
-        // const checkoutData = {
-        //   cartItems: cartItems.map((cartItem) => ({
-        //     menuItemId: cartItem._id,
-        //     name: cartItem.name,
-        //     quantity: cartItem.quantity.toString(),
-        //   })),
-        //   restaurantId: restaurant._id,
-        //   deliveryDetails: {
-        //     name: userFormData.name,
-        //     addressLine1: userFormData.addressLine1,
-        //     city: userFormData.city,
-        //     country: userFormData.country,
-        //     email: userFormData.email as string,
-        //   },
-        // };
+        const checkoutData = {
+          cartItems: cartItems.map((cartItem) => ({
+            menuItemId: cartItem._id,
+            name: cartItem.name,
+            quantity: cartItem.quantity.toString(),
+          })),
+          restaurantId: restaurant._id,
+          deliveryDetails: {
+            name: userFormData.name,
+            addressLine1: userFormData.addressLine1,
+            city: userFormData.city,
+            country: userFormData.country,
+            email: userFormData.email as string,
+          },
+        };
     
-        // const data = await createCheckoutSession(checkoutData);
-        // window.location.href = data.url;
+        const data = await createCheckoutSession(checkoutData);
+        window.location.href = data.url;
     };
 
     if (isLoading || !restaurant) {
@@ -139,7 +140,7 @@ const DetailPage = () => {
                   <CheckoutButton
                     disabled={cartItems.length === 0}
                     onCheckout={onCheckout}
-                    // isLoading={isCheckoutLoading}
+                    isLoading={isCheckoutLoading}
                   />
                 </CardFooter>
               </Card>
